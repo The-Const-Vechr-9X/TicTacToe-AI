b06e5f3bb22b310476b94ceeae8b2f94b5b533e4
_9X_ <const.vechr@bk.ru>
2026-08-20 12:34:33 +0300

    feat(board): migrate from print to curses for interactive terminal UI

--- src/board.py
+++ src/board.py
@@ -1,19 +1,39 @@
+import curses
+
+
 class Board():
     def __init__(self, board_size: int = 9) -> None:
         self.board_size = board_size
         self.value_dict = {_: ' ' for _ in range(0, board_size)}
 
-    def draw(self) -> None:
-        index = 0
+    def draw(self, stdscr: curses.window) -> None:
         sqrt_board_size = int(self.board_size ** 0.5)
+        curses.curs_set(0)
 
-        for _ in range(sqrt_board_size):
-            print(" -" + "----" * sqrt_board_size)
+        while True:
+            stdscr.clear()
 
-            for _ in range(index, index + sqrt_board_size):
-                print(f" | {self.value_dict[index]}", end="")
-                index += 1
+            index = 0
+            y, x = 0, 0
 
-            print(" | ")
+            for _ in range(sqrt_board_size):
+                stdscr.addstr(y, x, " -" + "----" * sqrt_board_size)
+                y += 1
 
-        print(" -" + "----" * sqrt_board_size)
+                for _ in range(index, index + sqrt_board_size):
+                    stdscr.addstr(y, x, f" | {self.value_dict[index]}")
+
+                    index += 1
+                    x += 4
+
+                stdscr.addstr(y, x, " | ")
+
+                y += 1
+                x = 0
+
+            stdscr.addstr(y, x, " -" + "----" * sqrt_board_size)
+
+            key = stdscr.getch()
+
+            if key == ord('q'):
+                break
