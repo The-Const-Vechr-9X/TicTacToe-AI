abfc8f53c311d8df8cc3450102fb3c64ff4760a5
_9X_ <const.vechr@bk.ru>
2026-08-20 20:43:02 +0300

    feat: add main module to launch the game

---
+++ main.py
@@ -0,0 +1,10 @@
+import curses
+from src.game import Game
+
+
+def main(stdscr: curses.window):
+    game = Game()
+    game.show_main_menu(stdscr)
+
+if __name__ == "__main__":
+    curses.wrapper(main)
